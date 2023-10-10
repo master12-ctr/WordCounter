70ac941512441aecdec58626e53793f72a30436b
master12-ctr <128089351+master12-ctr@users.noreply.github.com>
2023-10-10 06:44:39 -0700

    Delete src/features directory

--- src/features/action/action.ts
+++
@@ -1,5 +0,0 @@
-export const CREATE_USER="CREATE_USER";
-export const RETRIEVE_USERS="RETRIEVE_USERS";
-export const UPDATE_USER="UPDATE_USER";
-export const DELETE_USER="DELETE_USER";
-export const DELETE_ALL_USERS="CREATE_USERS";
